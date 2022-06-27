373a12fcf4fe529b47c5017dd32a3d7ceacc8aa9
trevor-arthur <trevor.spaniola@protonmail.com>
2022-06-27 13:44:03 -0700

    Haul amount only appears on success now

--- heistGame/heistGame.go
+++ heistGame/heistGame.go
@@ -6,10 +6,6 @@ import (
 	"time"
 )
 
-// A heist game, that uses randomly generated numbers to determine the outcome.
-// See if you can escape, hopefully with a pile of cash!
-// https://github.com/trevor-arthur
-
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	isHeistOn := true
@@ -32,22 +28,21 @@ func main() {
 		switch leftSafely {
 		case 0:
 			isHeistOn = false
+			fmt.Println("Why did we rob the bank next to the police station?")
 			fmt.Println("BUSTED: The cops were waiting for you outside the doors!")
 		case 1:
 			isHeistOn = false
+			fmt.Println("Did somebody hear a door shut?")
 			fmt.Println("BUSTED: The vault door closed behind you, locking you inside!")
 		case 2:
 			isHeistOn = false
-			fmt.Println("To the roof!")
-			fmt.Println("BUSTED: You made it to the helicopter on the roof, but it looks like someone forgot to fuel it up!")
-		case 3:
-			fmt.Println("To the roof!")
-			fmt.Println("SUCCESS: You made it to the helicopter on the roof and safely got away!")
+			fmt.Println("Why won't the car start?!")
+			fmt.Println("BUSTED: You made it to the getaway car, but it looks like someone forgot to fuel it up!")
 		default:
 			fmt.Println("Start the getaway car!")
 			fmt.Println("SUCCESS: You made it to the getaway care and safely escaped!")
+			amtStolen := 10000 + rand.Intn(1000000)
+			fmt.Printf("Total haul from the heist: $%d\n", amtStolen)
 		}
-		amtStolen := 10000 + rand.Intn(1000000)
-		fmt.Printf("Total haul from the heist: $%d\n", amtStolen)
 	}
 }
